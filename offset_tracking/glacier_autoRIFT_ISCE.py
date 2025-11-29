1ab416f2bb6dee9dbd981f4aebad3cb9bfe37b36
Ayush Gupta <gupta101a12@gmail.com>
2025-11-29 13:13:14 -0800

    Fix import path for glacier_autoRIFT module

--- offset_tracking/glacier_autoRIFT_ISCE.py
+++ offset_tracking/glacier_autoRIFT_ISCE.py
@@ -182,7 +182,7 @@ try:
     from .glacier_stacking_autoRIFT import glacier_autoRIFT
 except ImportError:
     # Try global Autorift
-    from offset_tracking.glacier_stacking_autoRIFT import glacier_autoRIFT
+    from glacier_stacking_autoRIFT import glacier_autoRIFT
 except:
     raise Exception('Autorift does not appear to be installed.')
 
